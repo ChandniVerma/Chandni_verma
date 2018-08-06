73759d9aff43545bb045d80422da84394304b4d2
Chandni Verma <chandniverma0204@gmail.com>
2018-08-06 16:46:50 +0530

    Spring

--- KafkaWorks/src/com/training/Kafka/day2/ConsumerClient.java
+++
@@ -1,9 +0,0 @@
-package com.training.Kafka.day2;
-
-public class ConsumerClient {
-	public static void main(String[] args) {
-		Consumer consumer= new Consumer(KafkaProperties.TOPIC2,false);
-		consumer.start();
-	}
-
-}
